71dfc8b6836afed6eeeb271de0130c367a841bcd
Bladimir Pardo <bladimirpardomolina@gmail.com>
2025-03-29 22:20:09 -0300

    comentarios español

--- API/src/app/database.py
+++ API/src/app/database.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 import os
 
 class DatabaseConfig:
-    """Clase para manejar la configuración de la base de datos."""
     def __init__(self):
         self.host = os.getenv("DATABASE_HOST")
         self.port = os.getenv("DATABASE_PORT")
@@ -15,19 +14,11 @@ class DatabaseConfig:
     def get_database(self):
         return f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}"
 
-# Configuración de la base de datos
 db_config = DatabaseConfig()
-
-# Crear el motor de base de datos
 engine = create_engine(db_config.get_database())
-
-# Crear una clase base de modelo declarativo
 Base = declarative_base()
-
-# Crear una sesión de base de datos
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Función para obtener la sesión de la base de datos
 def get_db():
     db = SessionLocal()
     try:
